6f3d9479fea1eba43d9491f710f8ed8648bcc91e
Cade Gillem <cpgillem@gmail.com>
2024-05-12 00:42:12 -0400

    Delete misplaced widgets.

--- border.go
+++
@@ -1,7 +0,0 @@
-package widgets
-
-import "fyne.io/fyne/canvas"
-
-type Border struct {
-	canvas.Line
-}
--- feature.go
+++
@@ -1,7 +0,0 @@
-package widgets
-
-import "fyne.io/fyne/v2/widget"
-
-type Feature struct {
-	widget.BaseWidget
-}
